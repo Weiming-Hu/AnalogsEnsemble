c10cdeb90739c5cb618b33a2b31e8746f907ce35
Weiming-Hu <cosmos.weiming@outlook.com>
2018-09-11 19:36:56 -0400

    working on sim calc

--- app_similarityCalculator/similarityCalculator.cpp
+++ app_similarityCalculator/similarityCalculator.cpp
@@ -12,6 +12,27 @@
 
 using namespace std;
 
+// Program options:
+// https://theboostcpplibraries.com/boost.program_options
+// https://www.boost.org/doc/libs/1_58_0/doc/html/program_options/tutorial.html#idp337627504
+
+void runSimilarityCalculator() {
+    
+    // I need the following arguments
+    
+    // AnEnIO
+    string file_test_forecasts, file_search_forecasts, file_observations;
+    int verbose;
+    
+    // Read input data
+    string mode = "Read", file_type = "Forecasts";
+    Forecasts_array test_forecasts, search_forecasts;
+    Observations_array observations;
+    
+    AnEnIO io(mode, file_test_forecasts, file_type, verbose);
+    
+}
+
 int main(int argc, char** argv) {
     cout << "Calculates similarity metrics." << endl;
     
