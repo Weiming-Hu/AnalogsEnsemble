e086a7c30e879ba8c8468b317b2895f7311e94c8
Weiming <cosmos.weiming@outlook.com>
2018-10-03 21:36:41 -0400

    move files

--- generate-gh-page.sh
+++ generate-gh-page.sh
@@ -14,13 +14,7 @@ function doCompileCXX {
     echo "Compile for C++"
     mkdir buildC
     cd buildC
-
-    if [ -z "$TRAVIS_BRANCH" ]
-    then
-        cmake ..
-    else
-        CC=gcc-4.9 CXX=g++-4.9 cmake ..
-    fi
+    cmake ..
 
     make document
     cd html
@@ -81,44 +75,9 @@ rm README.md
 cp ../README.md .
 cd ..
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompileCXX
-    doCompileR
-    echo "Build is done. Please deploy the document manually."
-    exit 0
-fi
-
 # Call the compile functions
 doCompileCXX
 doCompileR
 
-# Now let's go have some fun with the cloned repo
-cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add -A .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploymentAutomation/deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval $(ssh-agent -s)
-ssh-add ../deploy_key
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO
+echo "Build is done. Please deploy the document manually. The updated gh-page is in the folder out/"
+exit 0
