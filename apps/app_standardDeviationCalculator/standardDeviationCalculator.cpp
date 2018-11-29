86ca2fe86843482db31531a9d42a7d42a9770797
Weiming-Hu <cosmos.weiming@outlook.com>
2018-11-29 18:29:42 -0500

    prepare MPI env

--- apps/app_standardDeviationCalculator/standardDeviationCalculator.cpp
+++ apps/app_standardDeviationCalculator/standardDeviationCalculator.cpp
@@ -21,8 +21,23 @@
 #include <sstream>
 #include <cmath>
 
+#if defined(_ENABLE_MPI)
+#include "netcdf_par.h"
+#include <mpi.h>
+#endif
+
 using namespace std;
 
 int main(int argc, char** argv) {
+
+    int world_size = -1, world_rank = -1;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    cout << "Rank " << world_rank << " out of " << world_size << endl;
+
+    MPI_Finalize();
     return 0;
 }
