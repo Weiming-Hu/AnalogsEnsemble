5ca2a071d0c5232a860b0182bb762c7762d74e43
Weiming-Hu <cosmos.weiming@outlook.com>
2018-11-29 17:47:01 -0500

    prepare standard deviation calc

---
+++ apps/app_standardDeviationCalculator/standardDeviationCalculator.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   standardDeviationCalculator.cpp
+ * Author: Weiming Hu <weiming@psu.edu>
+ *
+ * Created on November 29, 2018, 17:30
+ */
+
+/** @file */
+
+#include "AnEn.h"
+#include "AnEnIO.h"
+#include "CommonExeFunctions.h"
+
+#include "boost/program_options.hpp"
+#include "boost/multi_array.hpp"
+#include "boost/filesystem.hpp"
+#include "boost/exception/all.hpp"
+
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    return 0;
+}
