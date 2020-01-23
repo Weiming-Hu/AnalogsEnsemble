55ef4d41e01e58f7fa3ef1bc800df47180a30c8b
Weiming-Hu <weiming@psu.edu>
2020-01-23 18:56:06 -0500

    add config names
    Former-commit-id: f3e0bef71ef4da7debfda1e1e3b6366b6a86ab8b

---
+++ RAnalogs/RAnEn/src/ConfigNames.h
@@ -0,0 +1,43 @@
+/* 
+ * File:   ConfigNames.h
+ * Author: Weiming Hu <cervone@psu.edu>
+ *
+ * Created on January 23, 2020, 6:49 PM
+ */
+
+#ifndef CONFIGNAMES_H
+#define CONFIGNAMES_H
+
+#include <string>
+
+/**
+ * ConfigNames controls the names of the configuration list elements to
+ * be recognized.
+ */
+namespace ConfigNames {
+    static const std::string _OBS = "observations";
+    static const std::string _OBS_TIMES = "observation_times";
+    static const std::string _WEIGHTS = "weights";
+    static const std::string _CIRCULARS = "circulars";
+    static const std::string _FCST_TIMES = "forecast_times";
+    static const std::string _FLTS = "flts";
+    static const std::string _FCSTS = "forecasts";
+    static const std::string _TEST_TIMES = "test_times";
+    static const std::string _SEARCH_TIMES = "search_times";
+    static const std::string _VERBOSE = "verbose";
+    static const std::string _OBS_ID = "observation_id";
+    static const std::string _NUM_MEMBERS = "NUM_MEMBERS";
+    static const std::string _NUM_SIMS = "max_num_sims";
+    static const std::string _NUM_PAR_NA = "max_par_nan";
+    static const std::string _NUM_FLT_NA = "max_flt_nan";
+    static const std::string _FLT_RADIUS = "FLT_radius";
+    static const std::string _SAVE_SIMS = "preserve_similarity";
+    static const std::string _SAVE_SIMS_IND = "preserve_similarity_index";
+    static const std::string _SAVE_ANALOGS_IND = "preserve_analogs_index";
+    static const std::string _OPERATION = "operational";
+    static const std::string _CHECK_SEARCH_FUTURE = "check_search_future";
+    static const std::string _QUICK = "quick";
+}
+
+#endif /* CONFIGNAMES_H */
+
