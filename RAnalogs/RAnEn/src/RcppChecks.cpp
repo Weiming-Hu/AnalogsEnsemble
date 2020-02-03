7ad64322c99eab0a760a549235be72a30cfe0c9e
Weiming-Hu <weiming@psu.edu>
2020-02-03 12:19:33 -0500

    change ignored files
    Former-commit-id: ef99d6a8c74eae2807b30788416f00c2e1383bd4

---
+++ RAnalogs/RAnEn/src/RcppChecks.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: Weiming Hu <weiming@psu.edu>
+ *
+ * Created on August 15, 2018
+ *
+ */
+
+// [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::plugins(openmp)]]
+
+#include <Rcpp.h>
+#include "FunctionsR.h"
+
+using namespace Rcpp;
+
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
+
+//' @export
+// [[Rcpp::export]]
+bool checkOpenMP() {
+#ifndef _OPENMP
+    return false;
+#else
+    return true;
+#endif
+}
+
