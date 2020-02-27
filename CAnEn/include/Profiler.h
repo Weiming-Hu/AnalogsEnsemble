02ec0c8bbd80c4c5e7c5741b8e69c38623978162
Weiming-Hu <weiming@psu.edu>
2020-02-27 11:37:01 -0500

    add memory profiling functionality

--- CAnEn/include/Profiler.h
+++ CAnEn/include/Profiler.h
@@ -12,9 +12,40 @@
 #include <iostream>
 #include <ctime>
 
+// Include openmp header files if we are using it
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
+
+/*
+ * Based on different operating systems, we include different header files
+ * to read process memory usage. If the operating system is not supported,
+ * memory usage information will be disable by defining _UNKNOWN_OS_
+ * 
+ * This approach is adopted from David Robert Nadeau. References below:
+ * - 
+ * 
+ */
+#if defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+#include <unistd.h>
+#include <sys/resource.h>
+
+#if defined(__APPLE__) && defined(__MACH__)
+#include <mach/mach.h>
+
+#elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+#include <fcntl.h>
+#include <procfs.h>
+
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+#include <stdio.h>
+
+#endif
+
+#else
+#define _UNKNOWN_OS_
+#endif
+
 
 class Profiler {
 public:
@@ -32,6 +63,14 @@ private:
 
 #if defined(_OPENMP)
     std::vector<double> wtimes_;
+#endif
+    
+#ifndef _UNKNOWN_OS_
+    std::vector<std::size_t> peak_memory_;
+    std::vector<std::size_t> current_memory_;
+    
+    std::size_t getPeakRSS_();
+    std::size_t getCurrentRSS_();
 #endif
     
 };
--- CAnEn/src/Profiler.cpp
+++ CAnEn/src/Profiler.cpp
@@ -23,6 +23,11 @@ Profiler::Profiler(size_t num_sessions) {
 #if defined(_OPENMP)
     wtimes_.reserve(num_sessions);
 #endif
+
+#ifndef _UNKNOWN_OS_
+    peak_memory_.reserve(num_sessions);
+    current_memory_.reserve(num_sessions);
+#endif
 }
 
 Profiler::~Profiler() {
@@ -42,19 +47,24 @@ Profiler::log_time_session(const std::string& session_name) {
 #if defined(_OPENMP)
     wtimes_.push_back(omp_get_wtime());
 #endif
+
+#ifndef _UNKNOWN_OS_
+    peak_memory_.push_back(getPeakRSS_());
+    current_memory_.push_back(getCurrentRSS_());
+#endif
 }
 
 void
 Profiler::summary(std::ostream& os) const {
-    
+
     // Sanity check
     size_t num_sessions = session_names_.size();
     if (num_sessions <= 1) throw runtime_error("No sessions to report. Did you call start/log_time_session/end?");
     if (session_names_.front() != _SESSION_START) throw runtime_error("You didn't start the profiler by calling start");
-    
+
     // Define time variables
     long full_pduration = (ptimes_.back() - ptimes_.front()) / CLOCKS_PER_SEC;
-    
+
 #if defined(_OPENMP)
     long full_wduration = wtimes_.back() - wtimes_.front();
 #endif
@@ -71,33 +81,114 @@ Profiler::summary(std::ostream& os) const {
 
     os << "Session total:\t processor time (" << to_simple_string(ptime_duration) << ", 100%)"
 #if defined(_OPENMP)
-        << "\t wall time (" << to_simple_string(wtime_duration) << ", 100%)"
+            << "\t wall time (" << to_simple_string(wtime_duration) << ", 100%)"
 #endif
-        << endl;
-    
+            << endl;
+
     for (size_t session_i = 1; session_i < num_sessions; ++session_i) {
-        
+
         // Calculate processor time consumption
         clock_t ptime = (ptimes_[session_i] - ptimes_[session_i - 1]) / CLOCKS_PER_SEC;
         ptime_duration = seconds(ptime);
-        
+
 #if defined(_OPENMP)
         // Calculate wall time consumption
         long wtime = wtimes_[session_i] - wtimes_[session_i - 1];
         wtime_duration = seconds(wtime);
 #endif
-        
+
         os << "Session " << session_names_[session_i] <<
                 ":\t processor time (" << to_simple_string(ptime_duration)
-                << ", " << ptime/full_pduration * 100 << "%)"
+                << ", " << ptime / full_pduration * 100 << "%)"
 #if defined(_OPENMP)
                 << "\t wall time (" << to_simple_string(wtime_duration)
-                << ", " << wtime/full_wduration * 100 << "%)"
+                << ", " << wtime / full_wduration * 100 << "%)"
+#endif
+
+#ifndef _UNKNOWN_OS_
+                << "\t current memory (" << current_memory_[session_i] 
+                << " bytes)\t peak memory (" << peak_memory_[session_i]
+                << " bytes)"
 #endif
                 << endl;
-                
+
     }
     os << "**************** End of Profiler Summary *****************" << endl;
 
-    
 }
+
+
+#ifndef _UNKNOWN_OS_
+
+std::size_t Profiler::getCurrentRSS_() {
+
+#if defined(__APPLE__) && defined(__MACH__)
+    // OSX
+    struct mach_task_basic_info info;
+
+    mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
+
+    if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO,
+            (task_info_t) & info, &infoCount) != KERN_SUCCESS)
+        return (size_t) 0L; /* Can't access? */
+
+    return (size_t) info.resident_size;
+
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+    // Linux
+    long rss = 0L;
+    FILE* fp = NULL;
+
+    if ((fp = fopen("/proc/self/statm", "r")) == NULL) return (size_t) 0L; /* Can't open? */
+
+    if (fscanf(fp, "%*s%ld", &rss) != 1) {
+        fclose(fp);
+        return (size_t) 0L; /* Can't read? */
+    }
+
+    fclose(fp);
+
+    return (size_t) rss * (size_t) sysconf(_SC_PAGESIZE);
+
+#else
+    return (size_t) 0L; /* Unsupported. */
+#endif
+}
+
+std::size_t Profiler::getPeakRSS_() {
+
+#if (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+    // AIX and Solaris
+    struct psinfo psinfo;
+    int fd = -1;
+
+    if ((fd = open("/proc/self/psinfo", O_RDONLY)) == -1) return (size_t) 0L; /* Can't open? */
+
+    if (read(fd, &psinfo, sizeof (psinfo)) != sizeof (psinfo)) {
+        close(fd);
+        return (size_t) 0L; /* Can't read? */
+    }
+
+    close(fd);
+
+    return (size_t) (psinfo.pr_rssize * 1024L);
+
+#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+    // BSD, Linux, and OSX
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+
+#if defined(__APPLE__) && defined(__MACH__)
+    return (size_t) rusage.ru_maxrss;
+
+#else
+    return (size_t) (rusage.ru_maxrss * 1024L);
+#endif
+
+#else
+    // Unknown OS
+    return (size_t) 0L; /* Unsupported. */
+#endif
+}
+
+#endif
