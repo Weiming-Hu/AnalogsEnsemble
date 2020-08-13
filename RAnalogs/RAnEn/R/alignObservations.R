5fb82eb78f52cd55313378479a6c87744ed0eeb0
Weiming-Hu <weiming@psu.edu>
2020-08-13 16:07:45 -0400

    change silent to TRUE

--- RAnalogs/RAnEn/R/alignObservations.R
+++ RAnalogs/RAnEn/R/alignObservations.R
@@ -53,7 +53,7 @@
 #' @md
 #' @export
 alignObservations <- function(observations, observation.times, forecast.times,
-                              flts, return.na.index = F, show.progress = F, silent = F) {
+                              flts, return.na.index = F, show.progress = F, silent = T) {
   
   stopifnot(length(dim(observations)) == 3)
   stopifnot(dim(observations)[3] == length(observation.times))
