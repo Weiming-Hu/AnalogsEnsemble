c54d81bf4edfa55a4e5e0c1a54597f8a1feb4084
Weiming-Hu <weiming@psu.edu>
2021-04-07 18:17:08 -0400

    add 3D feature transformation

--- CAnEn/src/Forecasts.cpp
+++ CAnEn/src/Forecasts.cpp
@@ -32,8 +32,7 @@ using namespace std;
 static const vector<string> _EMBEDDING_TYPE_DESC = {
     "1-dimensional embedding [parameters]",
     "2-dimensional embedding [parameters, lead times]",
-    "2.5-dimensional embedding [general/vertical parameters, lead times]"
-
+    "3-dimensional embedding [parameters, stations, lead times]"
 };
 
 Forecasts::Forecasts() : Array4D(), BasicData() {
@@ -76,6 +75,32 @@ Forecasts::getFltTimeIndex(size_t timestamp) const {
 size_t
 Forecasts::getFltTimeIndex(Time const & time) const {
     return flts_.getIndex(time);
+}
+
+const Grid &
+Forecasts::getGrid() const {
+    return grid_;
+}
+
+void
+Forecasts::setGrid(const string & grid_file, Verbose verbose) {
+    if (verbose >= Verbose::Progress) cout << "Reading grid from " << grid_file << endl;
+    if (grid_file.empty()) throw runtime_error("Trying to read a forecast grid file but it is empty. Did you set up --fcst-grid?");
+    grid_.setup(grid_file);
+
+    if (verbose >= Verbose::Progress) cout << "Grid summary: " << grid_.summary() << endl;
+    if (verbose >= Verbose::Detail) cout << grid_ << endl;
+
+    size_t ngrids = grid_.nkeys();
+    if (ngrids > stations_.size()) {
+        throw runtime_error("More grids than stations!");
+    } else if (ngrids < stations_.size()) {
+        if (verbose >= Verbose::Warning) cerr << "Warning: There are " << ngrids << " station indices in the grid but "
+            << stations_.size() << " in the forecasts. Some stations will be filled with NAN after transformation!" << endl;
+    }
+
+    double max_key = grid_.getMaxKey();
+    if (max_key >= stations_.size()) throw runtime_error("Station index overflow in Grid!");
 }
 
 #if defined(_ENABLE_AI)
@@ -145,12 +170,19 @@ Forecasts::featureTransform(const string & embedding_model_path, Verbose verbose
     if (embedding_type == 0) {
         featureTransform_1D_(output, module, num_parameters, num_stations, num_times, num_lead_times, verbose);
 
-    } else if  (embedding_type == 1 || embedding_type == 2) {
+    } else if (embedding_type == 1) {
 
         if (flt_radius <= 0) throw runtime_error("Embedding lead time radius must be positive");
         if (verbose >= Verbose::Detail) cout << "Lead time radius for embedding: " << flt_radius << endl;
 
         featureTransform_2D_(output, module, flt_radius, num_parameters, num_stations, num_times, num_lead_times, verbose);
+
+    } else if (embedding_type == 2) {
+
+        if (flt_radius <= 0) throw runtime_error("Embedding lead time radius must be positive");
+        if (verbose >= Verbose::Detail) cout << "Lead time radius for embedding: " << flt_radius << endl;
+
+        featureTransform_3D_(output, module, flt_radius, num_parameters, num_times, num_lead_times, verbose);
 
     } else {
         throw runtime_error("Unknown embedding type from the embedding model (check attribute : embedding_type)");
@@ -193,6 +225,13 @@ Forecasts::featureTransform(const string & embedding_model_path, Verbose verbose
 
     const auto torch_data = output.data_ptr<float>();
 
+    // Embedding type 2 has a different station order
+    vector<size_t> stations_index;
+    if (embedding_type == 2) {
+        for (auto & it : grid_) stations_index.push_back(it.first);
+        num_stations = stations_index.size();
+    }
+
 #if defined(_OPENMP)
 #pragma omp parallel for schedule(static) collapse(3)
 #endif
@@ -202,9 +241,12 @@ Forecasts::featureTransform(const string & embedding_model_path, Verbose verbose
 
                 long int sample_i = (lead_time_i * num_stations + station_i) * num_times + time_i;
 
+                size_t station_index = station_i;
+                if (embedding_type == 2) station_index = stations_index[station_i];
+
                 for (long int feature_i = 0; feature_i < num_latent_features; ++feature_i) {
                     double value = torch_data[sample_i * num_latent_features + feature_i];
-                    setValue(value, feature_i, station_i, time_i, lead_time_i);
+                    setValue(value, feature_i, station_index, time_i, lead_time_i);
                 }
             }
         }
@@ -218,7 +260,7 @@ void
 Forecasts::featureTransform_1D_(at::Tensor & output, torch::jit::script::Module & module,
         long int num_parameters, long int num_stations, long int num_times, long int num_lead_times, Verbose verbose) {
 
-    // For embedding 0, 1-dimensional embedding with parameters only, the model only accpets features at a 
+    // For embedding #0, 1-dimensional embedding with parameters only, the model only accpets features at a 
     // single station and a single lead time. This is dictated by the structure of the Torch model.
     //
     const long int num_allowed_stations = 1;
@@ -266,7 +308,7 @@ void
 Forecasts::featureTransform_2D_(at::Tensor & output, torch::jit::script::Module & module, long int flt_radius,
         long int num_parameters, long int num_stations, long int num_times, long int num_lead_times, Verbose verbose) {
 
-    // For embedding 1, 2-dimensional embedding with parameters and lead times, the model accepts features 
+    // For embedding #1, 2-dimensional embedding with parameters and lead times, the model accepts features 
     // at a single station but continuous lead times. This is dictated by the structure of the Torch model.
     //
     const long int num_allowed_stations = 1;
@@ -335,6 +377,106 @@ Forecasts::featureTransform_2D_(at::Tensor & output, torch::jit::script::Module 
     output = at::cat(tensor_outputs, 0);
     return;
 }
+
+void
+Forecasts::featureTransform_3D_(at::Tensor & output, torch::jit::script::Module & module, long int flt_radius,
+        long int num_parameters, long int num_times, long int num_lead_times, Verbose verbose) {
+
+    // For embedding #2, 3-dimensional embedding with parameters, stations, and lead times, the model accepts features
+    // at nearby stations and continuous lead times. This is dictated by the structure of the Torch model.
+    //
+
+    if (verbose >= Verbose::Progress) cout << "Populating the tensor with 3-dimensional embeddings ([parameters, stations, lead times]) ..." << endl;
+
+    long int num_stations = grid_.nkeys();
+    if (num_stations  == 0) throw runtime_error("3D transformation requires a Grid. Call setGrid first!");
+
+    // Get the station indices from the grid
+    vector<size_t> stations_index;
+    for (auto & it : grid_) stations_index.push_back(it.first);
+
+    // Read the spatial mask size from model
+    int metric_height = -1;
+    int metric_width = -1;
+    auto attr_list = module.named_attributes();
+
+    for (auto it = attr_list.begin(); it != attr_list.end(); ++it) {
+        if ((*it).name == "input_width") metric_width = (*it).value.toInt();
+        if ((*it).name == "input_height") metric_height = (*it).value.toInt();
+    }
+
+    if (metric_height <= 0 || metric_width <= 0) throw runtime_error("Cannot find input_width and input_height from Torch model!");
+    if (verbose >= Verbose::Progress) cout << "The spatial mask has width " << metric_width << " and height " << metric_height << endl;
+
+    // Copy values and call Torch routines
+    vector<at::Tensor> tensor_outputs(num_lead_times * num_stations);
+    size_t counter = 0;
+
+
+#if defined(_OPENMP)
+#pragma omp parallel for schedule(static) collapse(2)
+#endif
+    for (long int lead_time_i = 0; lead_time_i < num_lead_times; ++lead_time_i) {
+        for (long int station_i = 0; station_i < num_stations; ++station_i) {
+
+            Matrix mask;
+            grid_.getRectangle(stations_index.at(station_i), mask, metric_width, metric_height, true);
+
+            // Calculate the window size
+            long int flt_left = lead_time_i - flt_radius;
+            long int flt_right = lead_time_i + flt_radius + 1;
+
+            if (flt_left < 0) flt_left = 0;
+            if (flt_right >= num_lead_times) flt_right = num_lead_times;
+
+            long int window_size = flt_right - flt_left;
+
+            // Flatten the array data into a long vector
+            vector<float> torch_data(num_times * num_parameters * metric_height * metric_width * window_size);
+            long int pos = 0;
+
+            for (long int time_i = 0; time_i < num_times; ++time_i) {
+                for (long int parameter_i = 0; parameter_i < num_parameters; ++parameter_i) {
+                    for (long int height_i = 0; height_i < metric_height; ++height_i) {
+                        for (long int width_i = 0; width_i < metric_width; ++width_i) {
+                            for (long int window_i = 0; window_i < window_size; ++window_i, ++pos) {
+                                torch_data[pos] = getValue(parameter_i, mask(height_i, width_i), time_i, flt_left + window_i);
+                            }
+                        }
+                    }
+                }
+            }
+
+            auto x = torch::from_blob(torch_data.data(), {num_times, num_parameters, metric_height, metric_width, window_size}, torch::TensorOptions());
+            auto add_cpp_routines = at::full({1}, true, at::kBool);
+
+            // Initialize an empty vector
+            std::vector<torch::jit::IValue> inputs;
+
+            inputs.push_back(x);
+            inputs.push_back(add_cpp_routines);
+
+            // Disabling gradient calculation for the current thread
+            torch::NoGradGuard no_grad;
+
+            // Execute the model
+            // Input dimensions: [times (samples), features, height, width, lead time window size]
+            //
+            tensor_outputs[lead_time_i * num_stations + station_i] = module.forward(inputs).toTensor();
+
+#pragma omp critical
+            if (verbose >= Verbose::Detail) {
+                counter++;
+                cout << '\r' << "Features transformed for " << counter << "/" << num_lead_times * num_stations;
+                cout.flush();
+            }
+        }
+    }
+
+    if (verbose >= Verbose::Detail) cout << '\r' << "Feature transformation complete!" << endl;
+    output = at::cat(tensor_outputs, 0);
+    return;
+}
 #endif
 
 Forecasts &
@@ -343,6 +485,7 @@ Forecasts::operator=(const Forecasts & rhs) {
     if (this != &rhs) {
         BasicData::operator=(rhs);
         flts_ = rhs.flts_;
+        grid_ = rhs.grid_;
     }
 
     return *this;
