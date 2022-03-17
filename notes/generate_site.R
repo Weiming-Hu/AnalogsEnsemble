db7d617f58f2bcdaf76884564be7ac21fad54715
Weiming-Hu <weiming@psu.edu>
2022-03-17 11:37:35 -0400

    fix R doc source URL

--- notes/generate_site.R
+++ notes/generate_site.R
@@ -12,4 +12,8 @@ library(devtools)
 # options(repos='http://cran.rstudio.com/')
 
 load_all()
-build_site()
+build_site(overrider = list(repo = list(url = list(
+  home = 'https://weiming-hu.github.io/AnalogsEnsemble/',
+  source = 'https://github.com/Weiming-Hu/AnalogsEnsemble/tree/master/RAnalogs/RAnEn/',
+  issue = 'https://github.com/Weiming-Hu/AnalogsEnsemble/issues/',
+  user = 'https://github.com/Weiming-Hu/'))))
