c27d1e11983acf35aa43f07a9b15d361127d2ece
Weiming-Hu <weiming@psu.edu>
2022-03-17 11:55:01 -0400

    use pkg config

--- notes/generate_site.R
+++ notes/generate_site.R
@@ -12,8 +12,5 @@ library(devtools)
 # options(repos='http://cran.rstudio.com/')
 
 load_all()
-build_site(overrider = list(repo = list(url = list(
-  home = 'https://weiming-hu.github.io/AnalogsEnsemble/',
-  source = 'https://github.com/Weiming-Hu/AnalogsEnsemble/tree/master/RAnalogs/RAnEn/',
-  issue = 'https://github.com/Weiming-Hu/AnalogsEnsemble/issues/',
-  user = 'https://github.com/Weiming-Hu/'))))
+build_site()
+
